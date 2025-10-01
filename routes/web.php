ea0693e7d7f9117f11c2aa8b5d380e8691387620
ansa1019 <ansa1019@gmail.com>
2025-10-01 16:02:52 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\EditController;
 use App\Http\Controllers\SettingController;
 use App\Http\Controllers\DemoAnalysisController; #模擬分析
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Response;#回傳圖片
+use Illuminate\Support\Facades\Response; #回傳圖片
 
 
 /*
@@ -22,27 +22,25 @@ use Illuminate\Support\Facades\Response;#回傳圖片
 |
 */
 
-    Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', [DashboardController::class, 'view'])->name('dashboard');
-    Route::get('/project', [ProjectController::class, 'view'])->name('project');
-    Route::get('/sample', [SampleController::class, 'view'])->name('sample');
-    Route::get('/analysis', [AnalysisController::class, 'new'])->name('new_analysis');
-    Route::get('/demo-analysis', [DemoAnalysisController::class, 'index'])->name('demo.analysis');
-    Route::post('/demo-analysis/run', [DemoAnalysisController::class, 'run'])->name('demo.analysis.run');
-    Route::get('/setting', [SettingController::class, 'personal'])->name('setting.personal');
-    Route::patch('/setting', [SettingController::class, 'update'])->name('setting.update');
-    Route::delete('/setting', [SettingController::class, 'destroy'])->name('setting.destroy');
-    Route::get('/edit', [EditController::class, 'view'])->name('edit');
-    Route::match(['get', 'post'], '/analysis/{id}', [AnalysisController::class, 'project'])->name('analysis');
-    Route::get('/demo_results/{path}', function ($path) {
+Route::get('/', [DashboardController::class, 'view'])->name('dashboard');
+Route::get('/project', [ProjectController::class, 'view'])->name('project');
+Route::get('/sample', [SampleController::class, 'view'])->name('sample');
+Route::get('/analysis', [AnalysisController::class, 'new'])->name('new_analysis');
+Route::get('/demo-analysis', [DemoAnalysisController::class, 'index'])->name('demo.analysis');
+Route::post('/demo-analysis/run', [DemoAnalysisController::class, 'run'])->name('demo.analysis.run');
+Route::get('/setting', [SettingController::class, 'personal'])->name('setting.personal');
+Route::patch('/setting', [SettingController::class, 'update'])->name('setting.update');
+Route::delete('/setting', [SettingController::class, 'destroy'])->name('setting.destroy');
+Route::get('/edit', [EditController::class, 'view'])->name('edit');
+Route::match(['get', 'post'], '/analysis/{id}', [AnalysisController::class, 'project'])->name('analysis');
+Route::get('/demo_results/{path}', function ($path) {
     $fullPath = base_path("Model/demo/demo_results/" . $path);
 
     if (!file_exists($fullPath)) {
         abort(404, "檔案不存在: " . $fullPath);
     }
 
-        return Response::file($fullPath);
-    })->where('path', '.*');
-});
+    return Response::file($fullPath);
+})->where('path', '.*');
 
 require __DIR__ . '/auth.php';
